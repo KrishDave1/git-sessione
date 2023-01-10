dfb5bbd64fd68af312274c795b024201d1f098ed
KrishDave1 <Krish.Dave@iiitb.ac.in>
2023-01-10 22:15:47 +0530

    Added q1 againgit status!

---
+++ q1.py
@@ -0,0 +1,14 @@
+nput())
+for i in range(0,n):
+        ele=int(input())
+            l.append(ele)
+
+            #sum=0
+            #for i in range(len(l)):
+            #    sum=sum+l[i]
+            #print(sum)
+
+            sum=0;
+            for i in range(len(l)):
+                    sum=sum + l[i]*l[i]
+                    print(sum)
